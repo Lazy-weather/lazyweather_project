9f55f36bd2b2a8d508ef4c1c2020c8ecad1a874b
Roman Chmirov <chmirov2006@gmail.com>
2023-12-04 10:15:55 +0100

    initial

--- users/models.py
+++ users/models.py
@@ -3,19 +3,19 @@ from django.urls import reverse
 
 
 class User(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%y/%m/%d/")
-    time_created = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Информация")
+    photo = models.ImageField(upload_to="photos/%y/%m/%d/", verbose_name="Фото")
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
+    is_published = models.BooleanField(default=True, verbose_name="Статус публикации")
 
     def __str__(self):
         return self.title
 
+    # Добавление кнопки "Добавить запись" в админ-панели
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
-
 
     class Meta:
         verbose_name = 'Пользователи сайта'
